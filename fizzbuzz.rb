fa84c5fc76fe17628441a3d498f068c6e9dd0352
Abijah Stokes <abijahstokes@gmail.com>
2018-09-13 22:37:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-
+  print fizz if fizz_3 = fizzbuzz(3)
+  print buzz if fizz_5 = fizzbuzz(5)
+  print fizzbuzz if fizz_15 = fizzbuzz(15)
 end
