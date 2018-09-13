ac908e6ab07e1bbf868e1ba22585bb3f1f3f1b81
Abijah Stokes <abijahstokes@gmail.com>
2018-09-13 23:06:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@
       "Fizz" # Go fizz
     elsif int % 5 == 0
       "Buzz"
-    elsif int % 3  && int % 5 
+    elsif int % 3 == 0 && int % 5 == 0
       "FizzBuzz"
     else int % 4 == 0
       nil
