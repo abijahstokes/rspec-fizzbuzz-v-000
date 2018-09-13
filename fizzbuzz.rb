1abcdca514f86490616bdf91537587d64577084b
Abijah Stokes <abijahstokes@gmail.com>
2018-09-13 22:41:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  print fizz if fizz_3 = fizzbuzz(3)
-  print buzz if fizz_5 = fizzbuzz(5)
-  print fizzbuzz if fizz_15 = fizzbuzz(15)
+  def fizzbuzz(int)
+    if int % 3 == 0 # if the number int is divisible by 3
+      "Fizz" # Go fizz
+    elsif int % 5 == 0
+      "Buzz"
+    else int % 15 == 0
+      "FizzBuzz" 
+      
 end
